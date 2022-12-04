544e85bbae3c1eac2b0d7d281b59dfdf1ba0962e
Dominic R <110837479+jD2R@users.noreply.github.com>
2022-12-04 18:57:38 -0500

    Update Day 3

--- Day 3/p1.py
+++ Day 3/p1.py
@@ -1,18 +1,33 @@
+
+## Advent of Code
+## Day 3, Problem 1
+## Dominic R. 2022
+
+## FILE HANDLING
+# open the file in reading mode
 file = open('input.txt', 'r')
+# grab the lines
 lines = file.readlines()
+# close the file
 file.close()
 
+# priority list rankings
 priority = '+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+# sum of all the priorities
 sum = 0
 
+# iterate through the lines
 for line in lines:
   n = len(line)
+  # slice the lines
   comp1 = line[0:n//2]
   comp2 = line[n//2:]
+  # find the common letter
   letter = "".join(list(set(comp1) & set(comp2)))
-  print(letter)
+  # find the letter in the priority string
   val = priority.find(str(letter));
+  # add the index in the priority string to the sum
   sum += val
-
+# output the sum of the priorities
 print(sum)
--- Day 3/p2.py
+++ Day 3/p2.py
@@ -1,9 +1,20 @@
+
+## Advent of Code
+## Day 3, Problem 2
+## Dominic R. 2022
+
+## FILE HANDLING
+# open the file in reading mode
 file = open('input.txt', 'r')
+# grab the lines
 lines = file.readlines()
+# close the file
 file.close()
 
+# priority string
 priority = '+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+# separates the raw input into rucksacks
 def separateRucksacks():
   rucksacks = [[]]
   count = 1
@@ -16,10 +27,12 @@ def separateRucksacks():
   rucksacks.pop()
   return rucksacks;
 
+# grab the rucksacks
 sacks = separateRucksacks();
 sum = 0
+# iterate through, find the common letter, and add the index to the sum
 for sack in sacks:
   val = list(set(sack[0]) & set(sack[1]) & set(sack[2]))[0]
   sum += priority.find(val)
-
+# output the sum of the priorities
 print(sum)
